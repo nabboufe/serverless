2ece3f861ad574236b608040605e8c2c2fc3fd6e
affect <nabil.boufeldja@gmail.com>
2023-12-15 22:44:29 +0100

    a

--- handler.py
+++ handler.py
@@ -39,9 +39,12 @@ def decode_escapes(s):
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
 
 def load_model():
-    global generator, default_settings
+    global simple_generator, lora
 
-    if not generator:
+    logging.info(f"generator: {not not simple_generator}, lora: {not not lora}")
+
+    if not simple_generator or not lora :
+        logging.info("simple generator")
         token_read = os.environ["HF_TOKEN"]
         HfFolder.save_token(token_read)
 
@@ -64,24 +67,27 @@ def load_model():
 
     return simple_generator, lora
 
-generator = None
+simple_generator = None
+lora = None
 default_settings = None
 
 def inference(event) -> Union[str, Generator[str, None, None]]:
 
     logging.info(event)
-    prompt = event["input"]
-    max_new_token = event["max_new_tokens"]
+    prompt = event["input"]["prompt"]
+    max_new_token = event["input"]["max_new_tokens"]
 
     settings = ExLlamaV2Sampler.Settings()
-    settings.temperature = event["temperature"]
-    settings.top_k = event["top_k"]
-    settings.top_p = event["top_p"]
-    settings.token_repetition_penalty = event["repetition_penalty"]
+    settings.temperature = event["input"]["temperature"]
+    settings.top_k = event["input"]["top_k"]
+    settings.top_p = event["input"]["top_p"]
+    settings.token_repetition_penalty = event["input"]["repetition_penalty"]
 
     simple_generator, lora = load_model()
+
+    logging.info(prompt, settings, max_new_token, lora)
     output = simple_generator.generate_simple(prompt, settings, max_new_token, loras = lora)
-    
-    return output
+
+    return output[len(prompt):]
 
 runpod.serverless.start({"handler": inference})
